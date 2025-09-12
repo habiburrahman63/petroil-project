f95d7d4baaefe52531a92b4103afb9c3452d2594
habiburrahman63 <habiburrahman90763@gamil.com>
2025-09-12 20:17:38 +0600

    slider gap

--- src/components/Imagecarosel/Imagecarosel.jsx
+++ src/components/Imagecarosel/Imagecarosel.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
-import Flex from '../Layout/Flex'
-
+import React from "react";
+import Flex from "../Layout/Flex";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const Imagecarosel = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    autoplay: 500,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+  };
   return (
-    <div >
-      <Flex className="gap-[30px]">
-        <img className='w-[25%]' src="/unsplash_1.png" alt="" />
-        <img className='w-[25%]' src="/unsplash_2.png" alt="" />
-        <img className='w-[25%]' src="/unsplash_3.png" alt="" />
-        <img className='w-[25%]' src="/unsplash_4.png" alt="" />
-      </Flex>
-    </div>
-  )
-}
+    <div>
+      {/* <Flex className="gap-[30px]">
 
-export default Imagecarosel
+      </Flex> */}
+      <Slider {...settings}>
+        <img className="w-[25%] px-[15px]" src="/unsplash_1.png" alt="" />
+        <img className="w-[25%] px-[15px]" src="/unsplash_2.png" alt="" />
+        <img className="w-[25%] px-[15px]" src="/unsplash_3.png" alt="" />
+        <img className="w-[25%] px-[15px]" src="/unsplash_4.png" alt="" />
+        <img className="w-[25%] px-[15px]" src="/unsplash_1.png" alt="" />
+        <img className="w-[25%] px-[15px]" src="/unsplash_2.png" alt="" />
+        <img className="w-[25%] px-[15px]" src="/unsplash_3.png" alt="" />
+        <img className="w-[25%] px-[15px]" src="/unsplash_4.png" alt="" />
+      </Slider>
+    </div>
+  );
+};
+
+export default Imagecarosel;
--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -4,16 +4,20 @@ import Button from "../Layout/Button";
 
 const Service = () => {
   return (
-    <div className="mt-[28px]">
+    <div className="mt-[28px] font-primary">
       <Flex className="justify-between items-center">
-        <div className="ml-[390px] mr-[150px]">
-          <h1 className="text-[64px] font-bold text-[#000000]">Our Services</h1>
-          <p className="text-[16px] font-medium text-[#6C6C6C] w-[405px]">
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout.
-          </p>
+        <div className="flex justify-end w-[50%] mr-[150px]">
+          <div>
+            <h1 className="text-[64px] font-bold text-[#000000]">
+              Our Services
+            </h1>
+            <p className="text-[16px] font-medium text-[#6C6C6C] w-[405px]">
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout.
+            </p>
+          </div>
         </div>
-        <div className="bg-[url(./assets/service.png)] w-[100%] pt-[147px] pb-[139px] relative z-[1] bg-no-repeat bg-center bg-cover">
+        <div className="bg-[url(./assets/service.png)] w-[50%] pt-[147px] pb-[139px] relative z-[1] bg-no-repeat bg-center bg-cover">
           <div className="absolute w-full h-full top-0 left-0  bg-[#000000]/60 z-[-1]"></div>
           <div className="ml-[116px]">
             <p className="text-[36px] font-bold text-[#FFFFFF]">
